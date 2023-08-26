0bebee6251259c8a25d0e058d90ecb92b5abbd37
Rodion-Bozhenko <rodion.bozhenko28@gmail.com>
2023-08-26 21:09:14 +0200

    Prototype repl

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"interpreter/repl"
+	"os"
+	"os/user"
+)
 
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	printfln("Hello %s! This is the Ligma programming language\n", user.Username)
+	printfln("Feel free to type in commands\n")
+
+	repl.Start(os.Stdin, os.Stdout)
+}
+
+func printfln(format string, a ...interface{}) {
+	fmt.Printf(format, a...)
 }
